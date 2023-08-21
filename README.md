9165dcd711be9f98a21a876ebb2c274ef00db640
“Arunthamizh” <arunthamizh.azhagiri@twilightitsolutions.com>
2023-08-21 18:40:18 +0530

    24th module 18 video completed

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ To get more help on the Angular CLI use `ng help` or go check out the [Angular C
 <!--  -->
 
 <!-- 24 Bonus Module -->
-<!-- completed 16th Video  -->
+<!-- completed 18th Video  -->
 
 <!-- NGRX -->
 First command
--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,4 +1,5 @@
 import { User } from '../user.model'
+import * as  AuthActions  from './auth.action';
 
 export interface state {
   user: User
@@ -8,6 +9,35 @@ const initialState: state = {
   user: null
 };
 
-export function authReducer(state = initialState, action) {
-  return state
+export function authReducer(state = initialState, action: AuthActions.AuthActions) {
+
+  switch (action.type) {
+    //  case 1
+    case AuthActions.LOGIN:
+      const user = new User(
+        action.payload.email,
+        action.payload.userId,
+        action.payload.token,
+        action.payload.expirationDate
+      );
+
+      return {
+        ...state,
+        user: user
+      };
+
+    // case 2
+    case AuthActions.LOGOUT:
+      return {
+        ...state,
+        user: null
+      }
+
+    // case default;
+    default:
+
+    return state
+  }
+
+
 }
